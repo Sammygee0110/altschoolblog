c1f579d773e9c0813abb8041f6f0410db96ea4ec
Sammygee0110 <you@example.com>
2023-11-01 22:01:18 +0100

    altered blog author

--- base/models.py
+++ base/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, auto_created=True)
+    email = models.EmailField()
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Topic(models.Model):
+    topic = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.topic
+class Blog(models.Model):
+    title = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
